9cfbbe758afb621c2a20e2b20a128d6725071605
허동혁 <missko30@pusan.ac.kr>
2024-02-08 20:07:44 +0900

    [level 2] Title: 큰 수 만들기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.py
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.py
@@ -1,12 +1,10 @@
 def solution(number, k):
-    i = 0
-    while k:
-        curIdx, nextIdx = i, (i+1)%len(number)
-        if number[curIdx]<number[nextIdx]:
-            number = number[:curIdx]+number[curIdx+1:]
-            k-=1
-            i = max(0, i-1)
-            continue
-        else:
-            i = (i+1)%len(number)
-    return number
+    stack = [number[0]]
+    for num in number[1:]:
+        while len(stack) > 0 and stack[-1] < num and k > 0:
+            k -= 1
+            stack.pop()
+        stack.append(num)
+    if k != 0:
+        stack = stack[:-k]
+    return ''.join(stack)
