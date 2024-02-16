a11e7acdab4c1c520222ae75dcbddbbde5f4c9d1
허동혁 <missko30@pusan.ac.kr>
2024-02-16 19:24:13 +0900

    [level 3] Title: 합승 택시 요금, Time: 0.47 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.py
+++ 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.py
@@ -1,13 +1,27 @@
-from itertools import product
+from heapq import heappush, heappop
 
 def solution(n, s, a, b, fares):
-    s, a, b = s - 1, a - 1, b - 1
-    d = [[100000000] * n for _ in range(n)]
-    for t, f, v in fares:
-        d[t - 1][f - 1] = d[f - 1][t - 1] = v
-    for t in range(n):
-        d[t][t] = 0
-    for k, i, j in product(range(n), repeat=3):
-        if d[i][j] > (l := d[i][k] + d[k][j]):
-            d[i][j] = l
-    return min([d[s][k] + d[k][a] + d[k][b] for k in range(n)])
+    INF = 10**9
+    graph = {i: {i:0} for i in range(n+1)}
+    for node1, node2, cost in fares:
+        graph[node1][node2] = cost
+        graph[node2][node1] = cost
+    
+    def djikstra(start):
+        res = [INF] * (n+1)
+        q = [(0, start)]
+        while q:
+            curCost, curNode = heappop(q)
+            if curCost>res[curNode]:
+                continue
+            else:
+                for nextNode, cost in graph[curNode].items():
+                    nextCost = curCost+cost
+                    if nextCost<res[nextNode]:
+                        res[nextNode]=nextCost
+                        heappush(q, (nextCost, nextNode))
+        return res
+    
+    ansGraph = {i: djikstra(i) for i in [s, a, b]}
+    answer = min([ansGraph[s][i]+ansGraph[a][i]+ansGraph[b][i] for i in range(n+1)])
+    return answer
