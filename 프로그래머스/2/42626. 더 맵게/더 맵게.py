f9173a99e43149c3d319d61be88723df69f84ff2
허동혁 <missko30@pusan.ac.kr>
2024-02-20 15:41:37 +0900

    [level 2] Title: 더 맵게, Time: 1322.82 ms, Memory: 67 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.py
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.py
@@ -1,20 +1,23 @@
-"""
-문제 유형 : 힙
-아이디어 :
-- 모든 음식의 스코빌 지수가 k 이상일 때 까지 반복하여 섞는다.
-    -> 힙을 이용하여 2개의 원소를 pop 후
-    -> 새로운 원소를 만들어 push
-- while len(q)>1, 종료 조건은 q[0]이 k>=일 때,
-"""
-from heapq import heappush, heappop, heapify
+from collections import deque
 def solution(scoville, K):
-    q = scoville
-    heapify(q)
+    inf = 10**9
+    q = deque(sorted(scoville))
+    mix = deque()
     answer = 0
-    while len(q)>=2:
-        a, b = heappop(q), heappop(q)
-        if a>=K:
+    while len(q)+len(mix)>=2:
+        if min(q[0] if q else inf, mix[0] if mix else inf)>=K:
             return answer
-        heappush(q, a+b*2)
-        answer += 1
-    return answer if q[0]>=K else -1
+        temp = []
+        for _ in range(2):
+            if q and not mix:
+                temp.append(q.popleft())
+            elif q and mix:
+                if q[0]<mix[0]:
+                    temp.append(q.popleft())
+                else:
+                    temp.append(mix.popleft())
+            elif not q and mix:
+                temp.append(mix.popleft())
+        mix.append(temp[0]+temp[1]*2)
+        answer+=1
+    return answer if mix[0]>=K else -1
