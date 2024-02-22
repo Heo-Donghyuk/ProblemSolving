9dba7eb83d6b69745233a7e3255f38f1287288ed
허동혁 <missko30@pusan.ac.kr>
2024-02-22 23:50:45 +0900

    [Gold IV] Title: 최단경로, Time: 624 ms, Memory: 62032 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.py
+++ 백준/Gold/1753. 최단경로/최단경로.py
@@ -1,28 +1,31 @@
-import sys, heapq
+import sys
+from collections import defaultdict
+from heapq import heappop, heappush
 input = sys.stdin.readline
-print = sys.stdout.write
 
 inf = 10**9
-v, e = map(int, input().split())
-start = int(input())
-graph = {i: {i:0} for i in range(v+1)}
-answer = [inf] * (v+1)
-for _ in range(e):
-    u, v, w = map(int, input().split())
-    if v not in graph[u] or graph[u][v]>w:
-        graph[u][v]=w
-    
-def djikstra(start):
+def dijkstra(start, graph, nodeNum):
+    result = [inf]*(nodeNum+1)
+    result[start]=0
     q = [(0, start)]
     while q:
-        cost, node = heapq.heappop(q)
-        if cost>answer[node]:
+        cost, node = heappop(q)
+        if result[node]<cost:
             continue
         else:
-            for i, b in graph[node].items():
-                if cost+b<answer[i]:
-                    answer[i]=cost+b
-                    q.append((answer[i], i))
-    for i in answer[1:]:
-        print((str(i) if i!=inf else 'INF')+'\n')
-djikstra(start)
+            for nextNode, nextCost in graph[node].items():
+                newCost = cost+nextCost
+                if result[nextNode]>newCost:
+                    result[nextNode]=newCost
+                    heappush(q, (newCost, nextNode))
+    return result[1:]
+
+v, e = map(int, input().split())
+start = int(input())
+graph = defaultdict(dict)
+for _ in range(e):
+    node1, node2, cost = map(int, input().split())
+    if node2 not in graph[node1] or graph[node1][node2]>cost:
+        graph[node1][node2]=cost
+for n in dijkstra(start, graph, v):
+    print(str(n) if n!=inf else 'INF')
