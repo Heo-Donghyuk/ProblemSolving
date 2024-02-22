dbe182b5f6caa061ab3a72a26df0328b283f563c
허동혁 <missko30@pusan.ac.kr>
2024-02-22 20:48:58 +0900

    [level 3] Title: 섬 연결하기, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.py
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.py
@@ -1,13 +1,10 @@
 def find(graph, node):
-    if graph[node]!=node:
-        graph[node]=find(graph, graph[node])
+    if graph[node]!=node: graph[node]=find(graph, graph[node])
     return graph[node]
 def union(graph, node1, node2):
     root1, root2 = find(graph, node1), find(graph, node2)
-    if root1<root2:
-        graph[root2]=root1
-    else:
-        graph[root1]=root2
+    if root1<root2: graph[root2]=root1
+    else: graph[root1]=root2
 def solution(n, costs):
     graph = [i for i in range(n)]
     answer = 0
