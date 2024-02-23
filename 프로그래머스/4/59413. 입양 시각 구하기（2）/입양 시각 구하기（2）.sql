3c20236abf34f5f45da8d7787a3c2284f876747d
허동혁 <missko30@pusan.ac.kr>
2024-02-23 20:31:57 +0900

    [level 4] Title: 입양 시각 구하기(2), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/59413. 입양 시각 구하기（2）/입양 시각 구하기（2）.sql
+++ 프로그래머스/4/59413. 입양 시각 구하기（2）/입양 시각 구하기（2）.sql
@@ -1,7 +1,23 @@
-SELECT HOUR, COUNT(ANIMAL_ID) AS COUNT
-FROM ( SELECT (ROW_NUMBER() OVER () - 1) AS HOUR
-       FROM ANIMAL_OUTS
-       LIMIT 24) AS 24NUM_TABLE 
-     LEFT JOIN ANIMAL_OUTS ON HOUR = HOUR(DATETIME)
+/*
+아이디어:
+- 값이 없는 컬럼도 만들어 줘야 한다.
+    - 어떻게 만들지? rownum over?
+        => 겨우겨우 recursive cte로 만들었다.
+- 시간별로 group by 후 count하고 이를 재귀 cte와 union하자
+    - 이후 hour 별로 group by 후 count는 최댓값을 출력하자
+조건:
+주의:
+- 서브쿼리의 조건으로 충분한지 확인
+- 중복 제거 로직이 필요한지 확인
+*/
+
+WITH RECURSIVE T AS (SELECT 0 AS NUM, 0 AS COUNT
+                     UNION SELECT NUM+1, COUNT FROM T WHERE NUM<23)
+SELECT HOUR, MAX(COUNT) AS COUNT
+FROM (SELECT HOUR(DATETIME) AS HOUR, COUNT(*) AS COUNT
+FROM ANIMAL_OUTS
+GROUP BY HOUR
+UNION SELECT * FROM T) AS UNION_T
 GROUP BY HOUR
 ORDER BY HOUR
+
