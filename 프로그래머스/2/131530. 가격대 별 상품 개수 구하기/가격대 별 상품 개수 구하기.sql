a4ddcf2980980f106d7fda13ec51293e0ebc19c8
허동혁 <missko30@pusan.ac.kr>
2024-02-23 20:47:32 +0900

    [level 2] Title: 가격대 별 상품 개수 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/131530. 가격대 별 상품 개수 구하기/가격대 별 상품 개수 구하기.sql
+++ 프로그래머스/2/131530. 가격대 별 상품 개수 구하기/가격대 별 상품 개수 구하기.sql
@@ -1,4 +1,14 @@
-SELECT PRICE DIV 10000 * 10000 AS PRICE_GROUP, COUNT(*) AS PRODUCTS
+/*
+아이디어:
+- group by로 그룹화 하자
+    - price를 10000으로 나눈 몫 * 10000을 그룹화의 키로 사용하자
+조건
+주의:
+- 중복 제거 필요한지
+- left join
+- 서브쿼리 밖에서 조건을 추가할 필요가 있는지
+*/
+SELECT FLOOR(PRICE/10000)*10000 AS PRICE_GROUP, COUNT(*) AS PRODUCTS
 FROM PRODUCT
-GROUP BY PRICE_GROUP
+GROUP BY FLOOR(PRICE/10000)*10000
 ORDER BY PRICE_GROUP
