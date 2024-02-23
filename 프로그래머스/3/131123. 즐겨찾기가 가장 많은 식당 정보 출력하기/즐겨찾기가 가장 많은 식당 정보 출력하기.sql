d2136df99dca884b097e6ac95572191d4d372a06
허동혁 <missko30@pusan.ac.kr>
2024-02-23 16:51:42 +0900

    [level 3] Title: 즐겨찾기가 가장 많은 식당 정보 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/131123. 즐겨찾기가 가장 많은 식당 정보 출력하기/즐겨찾기가 가장 많은 식당 정보 출력하기.sql
+++ 프로그래머스/3/131123. 즐겨찾기가 가장 많은 식당 정보 출력하기/즐겨찾기가 가장 많은 식당 정보 출력하기.sql
@@ -1,4 +1,17 @@
--- 코드를 입력하세요
-SELECT FOOD_TYPE, REST_ID, REST_NAME, FAVORITES FROM REST_INFO AS T1
-WHERE FAVORITES = (SELECT MAX(FAVORITES) FROM REST_INFO WHERE T1.FOOD_TYPE=FOOD_TYPE)
+/*
+아이디어:
+서브쿼리와 in을 사용하자
+- 서브쿼리에서 group by로 음식 종류별로 그룹화 후
+    - 즐겨찾기가 가장 많은 즐겨찾기 수를 select 하자 (max 이용)
+- 이후 where 절에서 해당 음식종류, 즐겨찾기 수가 서브쿼리 절에 존재하면 선택하자
+조건:
+- 음식종류별로 즐겨찾기수가 가장 많은 식당
+- 음식 종류, ID, 식당 이름, 즐겨찾기수
+- 음식 종류를 기준으로 내림차순 정렬
+*/
+SELECT FOOD_TYPE, REST_ID, REST_NAME, FAVORITES
+FROM REST_INFO
+WHERE (FOOD_TYPE, FAVORITES) IN (SELECT FOOD_TYPE, MAX(FAVORITES)
+                                FROM REST_INFO
+                                GROUP BY FOOD_TYPE)
 ORDER BY FOOD_TYPE DESC
