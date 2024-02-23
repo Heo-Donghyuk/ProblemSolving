d6dc434fd6f44dca112b4a44f4fb0f9c35e192b0
허동혁 <missko30@pusan.ac.kr>
2024-02-23 16:26:15 +0900

    [level 4] Title: 오프라인/온라인 판매 데이터 통합하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/131537. 오프라인／온라인 판매 데이터 통합하기/오프라인／온라인 판매 데이터 통합하기.sql
+++ 프로그래머스/4/131537. 오프라인／온라인 판매 데이터 통합하기/오프라인／온라인 판매 데이터 통합하기.sql
@@ -1,7 +1,21 @@
--- 코드를 입력하세요
+/*
+아이디어:
+- 온, 오프라인 테이블을 세로로 합친다.
+    -> union all 사용
+- 2022년 3월에 해당하는 원소만 선택한다.
+- userId가 null로 표시되는지 확인하고
+- 정렬한다.
+조건:
+- 2022년 3월 오프라인/온라인 상품 판매 데이터
+- 판매 날짜, 상품ID, 유저ID, 판매량
+- 오프라인 테이블의 user_id 값은 Null로 표시
+- 판매일 오름차순, id 오름차순, 유저 id 오름차순
+*/
 SELECT DATE_FORMAT(SALES_DATE, '%Y-%m-%d') AS SALES_DATE, PRODUCT_ID, USER_ID, SALES_AMOUNT
-FROM (SELECT SALES_DATE, PRODUCT_ID, USER_ID, SALES_AMOUNT FROM ONLINE_SALE
-      UNION
-      SELECT SALES_DATE, PRODUCT_ID, NULL AS USER_ID, SALES_AMOUNT FROM OFFLINE_SALE) AS TB
+FROM ONLINE_SALE
+WHERE SALES_DATE LIKE '2022-03%'
+UNION ALL
+SELECT DATE_FORMAT(SALES_DATE, '%Y-%m-%d') AS SALES_DATE, PRODUCT_ID, NULL AS USER_ID, SALES_AMOUNT
+FROM OFFLINE_SALE
 WHERE SALES_DATE LIKE '2022-03%'
 ORDER BY SALES_DATE, PRODUCT_ID, USER_ID
