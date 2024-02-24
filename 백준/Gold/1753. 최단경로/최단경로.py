edb52aa7a692bac4b1270fafd317448784a2278d
허동혁 <missko30@pusan.ac.kr>
2024-02-24 12:18:45 +0900

    [Gold IV] Title: 최단경로, Time: 600 ms, Memory: 61904 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.py
+++ 백준/Gold/1753. 최단경로/최단경로.py
@@ -1,24 +1,23 @@
 import sys
 from collections import defaultdict
-from heapq import heappop, heappush
+from heapq import *
 input = sys.stdin.readline
+INF = 10**9
 
-inf = 10**9
 def dijkstra(start, graph, nodeNum):
-    result = [inf]*(nodeNum+1)
-    result[start]=0
+    res = [INF]*(nodeNum+1)
+    res[start]=0
     q = [(0, start)]
     while q:
-        cost, node = heappop(q)
-        if result[node]<cost:
+        cost, curNode = heappop(q)
+        if cost>res[curNode]:
             continue
-        else:
-            for nextNode, nextCost in graph[node].items():
-                newCost = cost+nextCost
-                if result[nextNode]>newCost:
-                    result[nextNode]=newCost
-                    heappush(q, (newCost, nextNode))
-    return result[1:]
+        for nextNode, nextCost in graph[curNode].items():
+            newCost = cost+nextCost
+            if res[nextNode]>newCost:
+                res[nextNode]=newCost
+                heappush(q, (newCost, nextNode))
+    return res[1:]
 
 v, e = map(int, input().split())
 start = int(input())
@@ -27,5 +26,6 @@ for _ in range(e):
     node1, node2, cost = map(int, input().split())
     if node2 not in graph[node1] or graph[node1][node2]>cost:
         graph[node1][node2]=cost
-for n in dijkstra(start, graph, v):
-    print(str(n) if n!=inf else 'INF')
+    
+for dist in dijkstra(start, graph, v):
+    print(dist if dist!=INF else 'INF')
