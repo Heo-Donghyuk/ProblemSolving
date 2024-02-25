201985a100b281cb389a516cfb0272b244c934ad
허동혁 <missko30@pusan.ac.kr>
2024-02-25 17:50:17 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 1032 ms, Memory: 73172 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
@@ -13,6 +13,7 @@
 """
 import sys
 input = sys.stdin.readline
+print = sys.stdout.write
 read = lambda: input().split()
 
 n, m = map(int, read())
@@ -30,4 +31,4 @@ for row in range(1, n+1):
 for _ in range(m):
     x1, y1, x2, y2 = map(int, read())
     res = dictionary[x2][y2]-dictionary[x1-1][y2]-dictionary[x2][y1-1]+dictionary[x1-1][y1-1]
-    print(res)
+    print(str(res)+'\n')
