b900788b31dd0868543ce72cd0e51769aae52267
허동혁 <missko30@pusan.ac.kr>
2024-02-28 14:27:35 +0900

    [Gold V] Title: 토마토, Time: 1080 ms, Memory: 222500 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.py
+++ 백준/Gold/7569. 토마토/토마토.py
@@ -1,47 +1,63 @@
+"""
+유형: DFS/BFS
+아이디어:
+- 3차원 테이블을 만들고
+- 거기서 BFS/DFS
+주의:
+-시간복잡도
+-예외:
+    - 최초 익은 토마토가 여러개일 수 있다.
+"""
 import sys
 input = sys.stdin.readline
+from collections import deque
 
-def solution():
-
-    n, m, h = map(int, input().split())
-    board = [[[*map(int, input().split())] for _ in range(m)] for _ in range(h)]
-    cnt = 0
-    tomatoes = []
-    depth = -1
-
-    for k in range(h):
-        for i in range(m):
-            for j in range(n):
-                if board[k][i][j] == 0:
-                    cnt += 1
-                elif board[k][i][j] == 1:
-                    tomatoes.append((k, i, j))
-
-    while tomatoes:
-        depth += 1
-        temp = []
-        for k, i, j in tomatoes:
-            if k > 0 and board[k-1][i][j] == 0:
-                board[k-1][i][j] = 1
-                temp.append((k-1,i,j))
-            if i > 0 and board[k][i-1][j] == 0:
-                board[k][i-1][j] = 1
-                temp.append((k,i-1,j))
-            if j > 0 and board[k][i][j-1] == 0:
-                board[k][i][j-1] = 1
-                temp.append((k,i,j-1))
-            if k < h-1 and board[k+1][i][j] == 0:
-                board[k+1][i][j] = 1
-                temp.append((k+1,i,j))
-            if i < m-1 and board[k][i+1][j] == 0:
-                board[k][i+1][j] = 1
-                temp.append((k,i+1,j))
-            if j < n-1 and board[k][i][j+1] == 0:
-                board[k][i][j+1] = 1
-                temp.append((k,i,j+1))
-        cnt -= len(temp)
-        tomatoes = temp
-
-    return -1 if cnt else depth
-                
-print(solution())
+def sol():
+    m, n, h = map(int, input().split())
+    board = [[[0]*m for _ in range(n)] for __ in range(h)]
+    q = deque()
+    cost, remain = 0, 0
+    for i in range(h):
+        for j in range(n):
+            for k, val in enumerate(map(int, input().split())):
+                if val==0:
+                    remain+=1
+                    board[i][j][k] = 0
+                elif val==-1:
+                    board[i][j][k] = -1
+                else:
+                    q.append((0, i, j, k))
+                    board[i][j][k] = -1
+    
+    # BFS        
+    while q:
+        cost, x, y, z = q.popleft()
+        if 0<=x+1<h and 0<=y<n and 0<=z<m and board[x+1][y][z]==0:
+            board[x+1][y][z]=-1
+            remain-=1
+            q.append((cost+1, x+1, y, z))
+        if 0<=x-1<h and 0<=y<n and 0<=z<m and board[x-1][y][z]==0:
+            board[x-1][y][z]=-1
+            remain-=1
+            q.append((cost+1, x-1, y, z))
+        if 0<=x<h and 0<=y+1<n and 0<=z<m and board[x][y+1][z]==0:
+            board[x][y+1][z]=-1
+            remain-=1
+            q.append((cost+1, x, y+1, z))
+        if 0<=x<h and 0<=y-1<n and 0<=z<m and board[x][y-1][z]==0:
+            board[x][y-1][z]=-1
+            remain-=1
+            q.append((cost+1, x, y-1, z))
+        if 0<=x<h and 0<=y<n and 0<=z+1<m and board[x][y][z+1]==0:
+            board[x][y][z+1]=-1
+            remain-=1
+            q.append((cost+1, x, y, z+1))
+        if 0<=x<h and 0<=y<n and 0<=z-1<m and board[x][y][z-1]==0:
+            board[x][y][z-1]=-1
+            remain-=1
+            q.append((cost+1, x, y, z-1))
+        else:
+            continue
+    # 익지 않은 토마토 판별
+    print(-1 if remain else cost)
+sol()
