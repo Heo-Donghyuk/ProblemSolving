b4419078673b3e30170fa8c57d26c6fab856c82d
허동혁 <missko30@pusan.ac.kr>
2024-03-02 11:47:11 +0900

    [Gold III] Title: 앱, Time: 368 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/7579. 앱/앱.py
+++ 백준/Gold/7579. 앱/앱.py
@@ -1,13 +1,10 @@
 n, m = map(int, input().split())
-memory, costs = list(map(int, input().split())), list(map(int, input().split()))
-max_cost = sum(costs)
-
-# dp[i]: i 비용으로 얻을 수 있는 최대 메모리
-dp = [0] * (max_cost + 1)
-
-for mem, cost in zip(memory, costs):
-    for c in range(max_cost, cost - 1, -1):
-        dp[c] = max(dp[c], dp[c - cost] + mem)
-
-answer = next(i for i in range(max_cost + 1) if dp[i] >= m)
-print(answer)
+memories, costs = list(map(int, input().split())), list(map(int, input().split()))
+DP = [0]*(sum(costs)+1)
+for mem, cost in zip(memories, costs):
+    for i in range(len(DP)-1, cost-1, -1):
+        DP[i] = max(DP[i], DP[i-cost]+mem)
+for i, mem in enumerate(DP):
+    if mem>=m:
+        print(i)
+        break
