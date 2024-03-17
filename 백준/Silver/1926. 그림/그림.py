c59fce90965cca37205cbc683a8dc63368cb3c6b
허동혁 <missko30@pusan.ac.kr>
2024-03-17 14:56:12 +0900

    [Silver I] Title: 그림, Time: 500 ms, Memory: 34072 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.py
+++ 백준/Silver/1926. 그림/그림.py
@@ -1,43 +1,30 @@
-"""
-BFS로 다시 풀어보기
-아이디어:
-- 이동할 수 있는 다음 좌표를 큐에 집어넣자
-    - 큐에서 pop할 때 마다 면적을 1씩 늘리자.
-"""
 import sys
 input = sys.stdin.readline
 from collections import deque
 from itertools import product
 
-def BFS(x, y, board):
-    res, board[x][y]= 1, 0
-    direction = [(0, 1), (1, 0), (0, -1), (-1, 0)]
-    q = deque([(x, y)])
-    while q:
-        x, y = q.popleft()
-        for d in direction:
-            nextX, nextY = map(sum, zip((x, y), d))
-            if isValid(nextX, nextY, board):
-                q.append((nextX, nextY))
-                board[nextX][nextY]=0
-                res+=1
-    return res
-
-def isValid(x, y, board):
-    n, m = len(board), len(board[0])
-    if 0<=x<n and 0<=y<m and board[x][y]:
-        return True
-    return False
-                
 n, m = map(int, input().split())
-board = []
+table = []
+answer = [0, 0]
 for _ in range(n):
-    board.append(list(map(int, input().split())))
+    table.append(list(map(int, input().split())))
+
+q = deque()
+for i, j in product(range(n), range(m)):
+    if table[i][j]:
+        q.append((i, j))
+        answer[0]+=1
+        temp = 1
+        table[i][j] = 0
+        while q:
+            x, y = q.popleft()
+            for addX, addY in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
+                nx, ny = x+addX, y+addY
+                if 0<=nx<n and 0<=ny<m and table[nx][ny]:
+                    q.append((nx, ny))
+                    table[nx][ny] = 0
+                    temp+=1
+        answer[1] = max(temp, answer[1])
     
-count, area = 0, 0
-for x, y in product(range(n), range(m)):
-    if board[x][y]:
-        area = max(area, BFS(x, y, board))
-        count+=1
-print(count)
-print(area)
+print(answer[0])
+print(answer[1])
